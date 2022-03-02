2132d03b7e69abc7837bd46d566ab2cd057ec512
Chris Schmitt <chrs097@gmail.com>
2022-03-02 17:04:49 -0600

    Pushing content added from Meeting today

--- No-Phishing-Zone/background.js
+++ No-Phishing-Zone/background.js
@@ -1,6 +1,19 @@
+function wait(ms) {
+    var defer = $.Deferred();
+    setTimeout(function() { defer.resolve(); }, ms);
+    return defer;
+};
 function doStuffWithDOM(domContent) {
-    console.log("I received the following DOM content:\n" + domContent);
-    alert("I received the following DOM content:\n" + domContent);
+//    console.log("I received the following DOM content:\n" + domContent);
+//    console.log(jQuery);
+  // $ - Tell jQuery to filter out only p tags. Selector syntax
+
+      // Filters text for a keyword
+//    console.log($(domContent).filter(":contains('redfin')"))  //Insert JQuery Selector
+      // Search for class="nH aHU" which shows the header
+      console.log($(domContent).filter("div.aHU"));
+
+//    alert("I received the following DOM content:\n" + domContent);
     chrome.extension.getBackgroundPage().domContent = domContent;
 }
 
--- No-Phishing-Zone/content.js
+++ No-Phishing-Zone/content.js
@@ -4,7 +4,6 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text && (msg.text == "report_back")) {
         /* Call the specified callback, passing
            the web-pages DOM content as argument */
-
         sendResponse(document.all[0].outerHTML);
     }
 });
