cd5dc5b93efbba89a1485efb4b85aa8a02d8dab0
Chris Schmitt <chrs097@gmail.com>
2022-04-05 16:49:16 -0500

    Started using content.js instead of background.js

--- No-Phishing-Zone/content.js
+++ No-Phishing-Zone/content.js
@@ -1,9 +1,32 @@
-/* Listen for messages */
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    /* If the received message has the expected format... */
-    if (msg.text && (msg.text == "report_back")) {
-        /* Call the specified callback, passing
-           the web-pages DOM content as argument */
-        sendResponse(document.all[0].outerHTML);
-    }
-});
+var senderEmail;
+var senderName;
+var emailBody;
+
+window.addEventListener('load', function () {
+      setTimeout(() => {
+            senderEmail = document.getElementsByClassName("go").item(0);
+            console.log(senderEmail);
+//            ValidateEmail(senderEmail);
+            senderEmail.style.color = "red";
+//            senderEmail.style = "{ border-style: dotted; border-radius: 5px 5px 5px; border: 1px solid red; }";
+
+            // senderName = document.getElementsByClassName("gD");
+            // console.log(senderName.item(0));
+
+            // emailBody = document.getElementsByClassName("nH aHU");
+            // console.log(emailBody);
+        }, 400);
+
+})
+
+function ValidateEmail(mail)
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+    senderEmail.style.color = "green";
+  }
+    console.log("You have entered an invalid email address!")
+    senderEmail.style.color = "red";
+    return (false)
+}
