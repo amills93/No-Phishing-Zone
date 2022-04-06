d00f38401b7d853389c46c031cf5ece884e3410e
Chris Schmitt <chrs097@gmail.com>
2022-04-06 17:59:41 -0500

    Adding blacklist.txt and whitelist.txt as well as modifications to content.js based on what Dr. Hale talked about in 4/6/22 meeting

--- No-Phishing-Zone/content.js
+++ No-Phishing-Zone/content.js
@@ -4,29 +4,58 @@ var emailBody;
 
 window.addEventListener('load', function () {
       setTimeout(() => {
+            hrefTags = document.links;
+            console.log(hrefTags);
+
             senderEmail = document.getElementsByClassName("go").item(0);
             console.log(senderEmail);
-//            ValidateEmail(senderEmail);
-            senderEmail.style.color = "red";
+            ValidateEmail(senderEmail);
+
 //            senderEmail.style = "{ border-style: dotted; border-radius: 5px 5px 5px; border: 1px solid red; }";
 
             // senderName = document.getElementsByClassName("gD");
             // console.log(senderName.item(0));
 
-            // emailBody = document.getElementsByClassName("nH aHU");
-            // console.log(emailBody);
+//            emailNode = document.getElementsByClassName("nH aHU").item(0);
+//            emailNode.childnodes //recursively go down to see if any are type a and add to a list of "found links"
+
+            // jQuery does this as part of a Query Selector (top element.sub element)
+            // children[i].nodeType == 'a'
+            console.log(emailBody);
         }, 400);
 
 })
 
 function ValidateEmail(mail)
 {
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  emailToValidate = mail.innerText;
+  console.log(emailToValidate);
+/* Strips off the < and > from the string */
+  emailToValidate = emailToValidate.replace(/[><]/g, '');
+  console.log(emailToValidate);
+
+    /* Variable used to test the regex */
+//  TestemailToValidate = 'paypal@paypaL.com'
+//  const rfc22 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+
+/*
+  The Regex is used to validate an email address based on the parameters below.
+  It assumes that emails with any capital letters after the '@' symbol is invalid.
+  Phishers will attempt to subsitue a lower case 'l' for 'L' to trick a user.
+*/
+ if (/^\w+([\.-]?\w+)*@[a-z]+([\.-]?[a-z]+)*(\.[a-z]{2,3})+$/.test(emailToValidate))
   {
-    return (true)
     senderEmail.style.color = "green";
+    console.log("Valid email address!")
+    return (true)
   }
-    console.log("You have entered an invalid email address!")
+    console.log("Invalid email address!")
     senderEmail.style.color = "red";
     return (false)
+}
+
+function ValidateURLS(url)
+{
+  // Insert Data Here
 }
