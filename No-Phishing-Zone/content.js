c64544fe3bba2bfefc6f38cc09e70f4469e2f1bd
Chris Schmitt <chrs097@gmail.com>
2022-04-09 13:37:32 -0500

    Fixed issue in previous commit by removing the isBlackListed function to the validateEmailFunction and setting a timeout, not the best way to do this but it works for now and as intended. Program can now validate email domains based on the regex and if found on the blacklist

--- No-Phishing-Zone/content.js
+++ No-Phishing-Zone/content.js
@@ -24,12 +24,17 @@ window.addEventListener('load', function () {
         }, 400);
 })
 
-function ValidateEmail(mail)
+async function ValidateEmail(mail)
 {
   /* Takes the innerText of the mail object */
   emailToValidate = mail.innerText;
 //  console.log(emailToValidate);
-
+  /* Chrome Extensions require you to load a local textfile using chrom.runtime.getURL */
+  const url = chrome.runtime.getURL('blacklist.txt');
+  /* Stores the text file once loaded below */
+  var storedText;
+  /* Booblean value used to check if a URL is found in the blacklist.txt file */
+  var bool = false;
 /* Strips off the < and > from the string */
   emailToValidate = emailToValidate.replace(/[><]/g, '');
 //  console.log(emailToValidate);
@@ -41,86 +46,109 @@ function ValidateEmail(mail)
 
     /* Variable used to test the regex */
 //  TestemailToValidate = 'paypal@paypaL.com'
-  const rfc22 = /^\w+([\.-]?\w+)*@[a-z]+([\.-]?[a-z]+)*(\.[a-z]{2,3})+$/
 
 /*
   The Regex is used to validate an email address based on the parameters below.
   It assumes that emails with any capital letters after the '@' symbol is invalid.
-  Phishers will attempt to subsitue a lower case 'l' for 'L' to trick a user.
+  Phishers will attempt to subsitue a lower case 'l' for 'L' to trick a user. Is
+  based of rfc22
 */
-  var isBlackListed = blackListTest(emailDomain);
+  const rfc22 = /^\w+([\.-]?\w+)*@[a-z]+([\.-]?[a-z]+)*(\.[a-z]{2,3})+$/
 
-//  console.log(isBlackListed + " Before rfc22 check")
-  //console.log(!bool isBlackListed + " before rfc22");
-  /* Since blackListTest returns true if the word is on the blackListTest
-      we need it to be false in order for the if statement to fail */
+//  var isBlackListed = blackListTest(emailDomain);
 
-/* turn to !isBlackListed after demo */
- if (rfc22.test(emailToValidate) && isBlackListed)
-  {
-    senderEmail.style.color = "green";
-    console.log("Valid email address!")
-    return (true)
-  }
-    console.log("Invalid email address!")
-    senderEmail.style.color = "red";
-    return (false)
-}
-
-async function blackListTest(domain)
-{
-  // console.log(whitelist);
-  /* Reads in the blacklist.txt file */
-  const url = chrome.runtime.getURL('blacklist.txt');
-  var storedText;
-  var bool = false;
-
+/* Function used to load the blacklist.txt url set above in the Variable url
+and see if the email domain is in the list. Will set bool to true if found */
   await fetch(url)
   .then(function(response) {
     response.text().then(function(text) {
+      /* Splits the text file based on the \n character */
       storedText = text.split("\n");
       for (let i = 1; i < storedText.length; i++) {
     //          console.log(wordList[i]);
           /* If the word is NOT found indexOf returns a -1
           the will continue looping through the rest of the loop */
-          if (storedText[i].indexOf(domain) >= 0) {
+          if (storedText[i].indexOf(emailDomain) >= 0) {
               bool = true;
-              console.log(storedText[i] + " " + bool + " Inside fetch");
+              console.log("Found " + storedText[i]);
+//              console.log(storedText[i] + " " + bool + " Inside fetch");
               break;
           }
         }
     });
   });
-//    console.log(bool + " Before Return fetch");
-    return bool;
 
-//   await fetch(url)
-//     .then(function(response) {
-//       response.text().then(function(text) {
-//         storedText = text.split("\n");
-//         done();
-// //        return bool;
-//       });
-//     });
+  /* Since blackListTest returns true if the word is on the blackListTest
+      we need it to be false in order for the if statement to fail we also
+      need to set a timeout so it waits for the function above to execute. */
+  setTimeout(() => {
+   if (rfc22.test(emailToValidate) && !bool)
+    {
+      /* Email Domain is valid if it passes the regex and is not found on the blacklist */
+      senderEmail.style.color = "green";
+      console.log("Valid email address!")
+      return (true)
+    }
+      console.log("Invalid email address!")
+      senderEmail.style.color = "red";
+      return (false)
+  }, 400);
+}
+
+// async function blackListTest(domain)
+// {
+//   // console.log(whitelist);
+//   /* Reads in the blacklist.txt file */
+//   const url = chrome.runtime.getURL('blacklist.txt');
+//   var storedText;
+//   var bool = false;
 //
-//   function done() {
-//     for (let i = 1; i < storedText.length; i++) {
-//             /* If the word is NOT found indexOf returns a -1
-//             the will continue looping through the rest of the loop */
-//             if (storedText[i].indexOf(domain) >= 0) {
-//                 console.log(storedText[i]);
-//                 bool = true;
-//                 console.log(storedText[i] + " " + bool + " Inside fetch");
-//                 break;
-//             }
-//             else {
-//               bool = false;
-//             }
+//   await fetch(url)
+//   .then(function(response) {
+//     response.text().then(function(text) {
+//       storedText = text.split("\n");
+//       for (let i = 1; i < storedText.length; i++) {
+//     //          console.log(wordList[i]);
+//           /* If the word is NOT found indexOf returns a -1
+//           the will continue looping through the rest of the loop */
+//           if (storedText[i].indexOf(domain) >= 0) {
+//               bool = true;
+//               console.log(storedText[i] + " " + bool + " Inside fetch");
+//               break;
 //           }
 //         }
-//     console.log(bool + " Before Return fetch");
+//     });
+//   });
+// //    console.log(bool + " Before Return fetch");
 //     return bool;
-}
+//
+// //   await fetch(url)
+// //     .then(function(response) {
+// //       response.text().then(function(text) {
+// //         storedText = text.split("\n");
+// //         done();
+// // //        return bool;
+// //       });
+// //     });
+// //
+// //   function done() {
+// //     for (let i = 1; i < storedText.length; i++) {
+// //             /* If the word is NOT found indexOf returns a -1
+// //             the will continue looping through the rest of the loop */
+// //             if (storedText[i].indexOf(domain) >= 0) {
+// //                 console.log(storedText[i]);
+// //                 bool = true;
+// //                 console.log(storedText[i] + " " + bool + " Inside fetch");
+// //                 break;
+// //             }
+// //             else {
+// //               bool = false;
+// //             }
+// //           }
+// //         }
+// //     console.log(bool + " Before Return fetch");
+// //     return bool;
+// }
 
 // function whiteListTest(mail)
 // {
