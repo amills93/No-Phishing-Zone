28afd2de88b93373c2aedf636a4b9d644094223e
Chris Schmitt <chrs097@gmail.com>
2022-04-09 13:12:06 -0500

    Started working on isBlackListed function to validate email domain. Proving more difficult due to async and sync function falls will need to troubleshoot wed with Prof

--- No-Phishing-Zone/content.js
+++ No-Phishing-Zone/content.js
@@ -22,7 +22,6 @@ window.addEventListener('load', function () {
             /* Calls the ValidateURLS Function */
             ValidateURLS(emailBody);
         }, 400);
-
 })
 
 function ValidateEmail(mail)
@@ -34,17 +33,30 @@ function ValidateEmail(mail)
 /* Strips off the < and > from the string */
   emailToValidate = emailToValidate.replace(/[><]/g, '');
 //  console.log(emailToValidate);
+  emailDomain = emailToValidate.split("@")[1];
+  /* Domain Used for Testing   */
+//  emailDomain = "mainews.ru";
+
+  console.log(emailDomain);
 
     /* Variable used to test the regex */
 //  TestemailToValidate = 'paypal@paypaL.com'
-//  const rfc22 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  const rfc22 = /^\w+([\.-]?\w+)*@[a-z]+([\.-]?[a-z]+)*(\.[a-z]{2,3})+$/
 
 /*
   The Regex is used to validate an email address based on the parameters below.
   It assumes that emails with any capital letters after the '@' symbol is invalid.
   Phishers will attempt to subsitue a lower case 'l' for 'L' to trick a user.
 */
- if (/^\w+([\.-]?\w+)*@[a-z]+([\.-]?[a-z]+)*(\.[a-z]{2,3})+$/.test(emailToValidate))
+  var isBlackListed = blackListTest(emailDomain);
+
+//  console.log(isBlackListed + " Before rfc22 check")
+  //console.log(!bool isBlackListed + " before rfc22");
+  /* Since blackListTest returns true if the word is on the blackListTest
+      we need it to be false in order for the if statement to fail */
+
+/* turn to !isBlackListed after demo */
+ if (rfc22.test(emailToValidate) && isBlackListed)
   {
     senderEmail.style.color = "green";
     console.log("Valid email address!")
@@ -55,15 +67,90 @@ function ValidateEmail(mail)
     return (false)
 }
 
+async function blackListTest(domain)
+{
+  // console.log(whitelist);
+  /* Reads in the blacklist.txt file */
+  const url = chrome.runtime.getURL('blacklist.txt');
+  var storedText;
+  var bool = false;
+
+  await fetch(url)
+  .then(function(response) {
+    response.text().then(function(text) {
+      storedText = text.split("\n");
+      for (let i = 1; i < storedText.length; i++) {
+    //          console.log(wordList[i]);
+          /* If the word is NOT found indexOf returns a -1
+          the will continue looping through the rest of the loop */
+          if (storedText[i].indexOf(domain) >= 0) {
+              bool = true;
+              console.log(storedText[i] + " " + bool + " Inside fetch");
+              break;
+          }
+        }
+    });
+  });
+//    console.log(bool + " Before Return fetch");
+    return bool;
+
+//   await fetch(url)
+//     .then(function(response) {
+//       response.text().then(function(text) {
+//         storedText = text.split("\n");
+//         done();
+// //        return bool;
+//       });
+//     });
+//
+//   function done() {
+//     for (let i = 1; i < storedText.length; i++) {
+//             /* If the word is NOT found indexOf returns a -1
+//             the will continue looping through the rest of the loop */
+//             if (storedText[i].indexOf(domain) >= 0) {
+//                 console.log(storedText[i]);
+//                 bool = true;
+//                 console.log(storedText[i] + " " + bool + " Inside fetch");
+//                 break;
+//             }
+//             else {
+//               bool = false;
+//             }
+//           }
+//         }
+//     console.log(bool + " Before Return fetch");
+//     return bool;
+}
+
+// function whiteListTest(mail)
+// {
+//   // console.log(whitelist);
+//   const url = chrome.runtime.getURL('email-whitelist.txt');
+//
+//   fetch(url).then(function(response) {
+//       return response.text().then(function(text) {
+//         const word = text.split("\n");
+//         for (let i = 1; i < word.length; i++) {
+//           console.log(word[i]);
+//           if (mail.test(word[i])) {
+//             return (true);
+//           }
+// //          return (mail.test(text[i]));
+//         }
+//       });
+//     });
+// }
+
 function ValidateURLS(url)
 {
   /* Creates a NodeList of all the 'a' tags in the URL Object */
   const myNodeList = url.querySelectorAll("a");
-  console.log(myNodeList);
+//  console.log(myNodeList);
 
   /* Loops through each a tag in the NodeList and
     attempts to validate it */
   for (let i = 0; i < myNodeList.length; i++) {
-    console.log(myNodeList[i].href);
+//    console.log(myNodeList[i].href);
+    myNodeList[i].style.color = "red";
   }
 }
